e0eac8a192a2f967a75e74e0ea64280673c4f5ac
iVesperX <aermias@live.com>
2018-06-18 13:24:04 -0400

    added files

---
+++ src/test/java/com/aermias/jdbc_testing/JdbcTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aermias.jdbc_testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
